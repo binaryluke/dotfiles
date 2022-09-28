b098ebbb5e078cbbea082eab25fc680465cb076c
Luke Howard <3931305+binaryluke@users.noreply.github.com>
2022-09-28 16:28:37 +0800

    Setup telescope for all the awesomeness

--- nvim/lua/binaryluke/packer.lua
+++ nvim/lua/binaryluke/packer.lua
@@ -1,5 +1,18 @@
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
+  -- lodash of neovim
+  use 'nvim-lua/plenary.nvim'
+
+  -- find stuff
+  -- todo: read telescope README.md and install optional deps like fd and fzf-native
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- color scheme
+  use 'folke/tokyonight.nvim'
 end)
 
