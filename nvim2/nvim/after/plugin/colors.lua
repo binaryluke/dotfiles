63d268353dfecd4c39c172038b0a05f89ce40048
Luke Howard <3931305+binaryluke@users.noreply.github.com>
2023-06-13 10:35:41 +1000

    Remove previous nvim configuration

--- nvim2/nvim/after/plugin/colors.lua
+++
@@ -1,8 +0,0 @@
-vim.opt.termguicolors = true
-
-require("tokyonight").setup({
-  style = "moon",
-  transparent = true,
-})
-
-vim.cmd('colorscheme tokyonight')
--- nvim2/nvim/init.lua
+++
@@ -1,5 +0,0 @@
--- Good overview of options: https://vonheikemen.github.io/devlog/tools/build-your-first-lua-config-for-neovim/
--- Good initial setup video: https://www.youtube.com/watch?v=w7i4amO_zaE
-require("binaryluke/options")
-require("binaryluke/keymaps").mainKeymaps()
-require("binaryluke/packer")
