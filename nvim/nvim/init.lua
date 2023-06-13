d94a889dc88708ada0ba1eb53feab968b0620743
Luke Howard <3931305+binaryluke@users.noreply.github.com>
2023-06-13 10:35:02 +1000

    Fzf and rg configuration for telescope

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -244,7 +244,26 @@ require('telescope').setup {
         ['<C-d>'] = false,
       },
     },
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '--hidden',
+      '--glob=!.git/',
+    },
   },
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+    }
+  }
 }
 
 -- Enable telescope fzf native, if installed
